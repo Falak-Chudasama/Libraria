7e55c1a28ea12f4728da3ac401ab9ad50dde3f01
Falak-Chudasama <falakchudasama7766@gmail.com>
2025-03-22 11:33:26 +0530

    Enhance user profile page: update styles, add new SVG icons, and improve layout structure

--- frontend/pages/user/profile.js
+++ frontend/pages/user/profile.js
@@ -17,6 +17,8 @@ const booksRead = document.getElementById('books-read');
 const favGenre = document.getElementById('fav-genre');
 const currentBook = document.getElementById('current-book');
 const favBook = document.getElementById('fav-book');
+const addressPen = document.getElementById('address-pen');
+const bioPen = document.getElementById('bio-pen');
 
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -28,6 +30,18 @@ document.addEventListener('DOMContentLoaded', () => {
     lastName.innerText = 'Morgan';
     email.innerText = 'arthur@gmail.com';
     phoneNumber.innerText = '+91 12345 67890';
+
+    house.innerText = 'house';
+    street.innerText = 'broadway';
+    city.innerText = 'Los Angeles';
+    state.innerText = 'California';
+
+    bio.innerText = 'Arthur Morgan was a gunslinger, artist, and reluctant philosopher who roamed the American West in the late 19th century. Born into a life of hardship, Arthur became a central figure in the Van der Linde gang—a group of outlaws chasing freedom in a world rapidly closing in. ';
+
+    booksRead.innerText = '26';
+    favGenre.innerText = 'Self Help';
+    currentBook.innerText = 'The Psychology of Money';
+    favBook.innerText = 'Shri Bhagwat Geeta';
 });
 
 homeBtn.addEventListener('click', (event) => {
